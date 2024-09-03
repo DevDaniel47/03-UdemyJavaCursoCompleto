b95acf78551c29211031581f054038c0f7a0a5bf
Daniel Carneiro <Daniel Carneiro@DanielWindows11>
2024-09-03 19:53:19 -0300

    Primeiro commit via Eclipse

---
+++ UdemyJavaCursoCompleto/src/quatro/sessao/Main.java
@@ -0,0 +1,12 @@
+package quatro.sessao;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello Word teste");
+		
+	}
+
+}
